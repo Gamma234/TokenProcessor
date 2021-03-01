2397bdcddac49a62461e0d6e28f1e1a81ed475f9
Gamma234 <45692067+Gamma234@users.noreply.github.com>
2021-03-01 15:00:47 +0800

    Add files via upload

---
+++ code/TokenProcessor_v2.cpp
@@ -0,0 +1,23 @@
+﻿# include "TokenProcessor.h"
+
+int main() {
+
+    TokenProcessor * tp = new TokenProcessor() ;
+
+    std::fstream inFile( "../../test_input/test01.txt", std::ios::in ) ;
+
+    if ( inFile ) {
+        tp->CutFile( inFile ) ;
+        tp->Output() ;
+        inFile.close() ;
+    } // if
+    else {
+        std::cout << "The file can not open !" << "\n" ;
+    } // else
+
+    delete tp ;
+    tp = NULL ;
+
+} // main()
+
+
